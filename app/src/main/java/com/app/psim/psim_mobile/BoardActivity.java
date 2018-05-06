ad9306f20ce8a83e9df9fc0bf2b05de01c754c83
Pawel.Stolarczyk <Pawel.Stolarczyk.96@gmail.com>
2018-05-06 02:28:59 +0200

    - Połączono Menu Dodawania Postów i Menu Wyszukiwania z Ekranem Głównym - W Ekranie Głównym dodano szufladę z podmenu

--- app/src/main/java/com/app/psim/psim_mobile/BoardActivity.java
+++ app/src/main/java/com/app/psim/psim_mobile/BoardActivity.java
@@ -1,8 +1,20 @@
 package com.app.psim.psim_mobile;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.app.psim.psim_mobile.components.BoardListViewAdapter;
 
@@ -12,6 +24,8 @@ import java.util.List;
 public class BoardActivity extends AppCompatActivity {
 
     private ListView postListView;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +48,56 @@ public class BoardActivity extends AppCompatActivity {
         list.add(new Post("Hekken Mekken", null, "Das is a nice description", "", "Xemur"));
         //END DEBUG
 
-        postListView = (ListView)findViewById(R.id.postListView);
+
+        postListView = (ListView)findViewById(R.id.board_postListView);
         BoardListViewAdapter adapter = new BoardListViewAdapter(this, R.layout.view_board_post, list);
         postListView.setAdapter(adapter);
+        postListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        Toolbar toolbar = findViewById(R.id.board_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.board_drawer);
+        navigationView = findViewById(R.id.board_navigationView);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        drawerLayout.closeDrawers();
+                        Intent intent = null;
+                        switch (item.getItemId())
+                        {
+                            case R.id.drawer_addpost:
+                                intent = new Intent(getBaseContext(), AddContentActivity.class);
+                                break;
+                            case R.id.drawer_search:
+                                intent = new Intent(getBaseContext(), BrowseActivity.class);
+                                break;
+                        }
+                        if(intent != null)
+                            startActivity(intent);
+                        return true;
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/app/psim/psim_mobile/Post.java
+++ app/src/main/java/com/app/psim/psim_mobile/Post.java
@@ -1,12 +1,16 @@
 package com.app.psim.psim_mobile;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Xemur on 01.05.2018.
  */
 
-public class Post {
+public class Post implements Parcelable {
 
     public Post(String title, List<String> tags, String description, String attachment, String author) {
         Title = title;
@@ -15,10 +19,48 @@ public class Post {
         Attachment = attachment;
         Author = author;
     }
+    private Post(Parcel data)
+    {
+        Title = data.readString();
+        Description = data.readString();
+        Attachment = data.readString();
+        Author = data.readString();
+        Tags = new ArrayList<String>();
+        int tagsSize = data.readInt();
+        for (int i = 0; i < tagsSize; ++i)
+            Tags.add(data.readString());
+    }
 
     public String Title;
     public List<String> Tags;
     public String Description;
     public String Attachment;
     public String Author;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(Title);
+        dest.writeString(Description);
+        dest.writeString(Attachment);
+        dest.writeString(Author);
+        dest.writeInt(Tags.size());
+        for (int i = 0; i < Tags.size(); ++i)
+            dest.writeString(Tags.get(i));
+    }
+
+    public static final Parcelable.Creator<Post> CREATOR = new Parcelable.Creator<Post>(){
+        public Post createFromParcel(Parcel data)
+        {
+            return new Post(data);
+        }
+        public Post[] newArray(int size)
+        {
+            return new Post[size];
+        }
+    };
 }
